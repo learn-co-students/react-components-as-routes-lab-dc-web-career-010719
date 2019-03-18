e4a5cc73c3fcbb199e1d7bc841c0e1acbcdbe11e
Ben Yellin <byellindc@gmail.com>
2019-03-18 12:38:23 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,16 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+        return (
+          <div key={actor.name}>
+            <h3>{actor.name}</h3>
+            <ul>{actor.movies.map(m => {
+              return <li key={m}>{m}</li>
+            })}</ul>
+          </div>)
+      })}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1> 
+      {movies.map(movie => {
+        return (
+          <div key={movie.title}>
+            <h3>{movie.title} - {movie.time} mins</h3>
+            <ul>{movie.genres.map(g => {
+              return <li key={g}>{g}</li>
+            })}</ul>
+          </div>)
+      })}
     </div>
   );
 };
